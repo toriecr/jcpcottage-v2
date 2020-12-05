ace04b91338c3a5591aec1c0fc7ec018cebffd23
toriecr <toriecr@gmail.com>
2020-12-04 20:51:07 -1000

    Clean up Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,18 +2,20 @@ import React from "react"
 
 const Footer = () => {
   return <footer>
-    <div className="footer-desc">
-      <p>This is the place we call home. This is the place we call home. This is the place we call home.</p>
-    </div>
-    <div className="footer-contact">
-      <h3>Contact Us</h3>
-      <p>14241 La Mirada St. Victorville, CA 92392</p>
-      <p>(Tel): 949-769-9626</p>
-      <p>(Fax): 1-800-457-9981</p>
-      <p>(Email): patlee@jcpcottage.com</p>
-    </div>
-    <div className="footer-links">
-      <p>This is the place we call home. This is the place we call home. This is the place we call home.</p>
+    <div className="footer-container">
+      <div className="footer-desc">
+        <p>This is the place we call home. This is the place we call home. This is the place we call home.</p>
+      </div>
+      <div className="footer-contact">
+        <h3>Contact Us</h3>
+        <p>14241 La Mirada St. Victorville, CA 92392</p>
+        <p>(Tel): 949-769-9626</p>
+        <p>(Fax): 1-800-457-9981</p>
+        <p>(Email): patlee@jcpcottage.com</p>
+      </div>
+      <div className="footer-links">
+        <p>This is the place we call home. This is the place we call home. This is the place we call home.</p>
+      </div>
     </div>
   </footer>
 }
