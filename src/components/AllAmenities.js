3c319491845dd897af8b1b5185341da020d65dbf
toriecr <toriecr@gmail.com>
2020-12-07 18:36:28 -1000

    Add Amenities page

---
+++ src/components/AllAmenities.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Title from "./Title"
+
+const AllAmenities = () => {
+  return <section className="all-amenities">
+    <Title title="All Amenities" />
+    <div className="underline"></div>
+  </section>
+}
+
+export default AllAmenities;
