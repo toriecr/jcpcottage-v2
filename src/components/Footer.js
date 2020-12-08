9b6e28507bbf4777c72e8fa2aab78c9938961006
toriecr <toriecr@gmail.com>
2020-12-07 16:18:54 -1000

    Style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,27 @@
 import React from "react"
+import SocialLinks from "../constants/socialLinks"
+import { BsHouseDoor } from "react-icons/bs"
 
 const Footer = () => {
   return <footer>
     <div className="footer-container">
       <div className="footer-desc">
-        <p>This is the place we call home. This is the place we call home. This is the place we call home.</p>
+        <div className="footer-icon">
+          <BsHouseDoor></BsHouseDoor>
+        </div>
+        <h3>JCP Cottage</h3>
+        <p>JCP Cottage is a residential, non-medical care facility for the elderly. We bridge the gap between independent living and skilled nursing care.</p>
       </div>
       <div className="footer-contact">
         <h3>Contact Us</h3>
         <p>14241 La Mirada St. Victorville, CA 92392</p>
-        <p>(Tel): 949-769-9626</p>
-        <p>(Fax): 1-800-457-9981</p>
-        <p>(Email): patlee@jcpcottage.com</p>
+        <p>Phone: 949-769-9626</p>
+        <p>Fax: 1-800-457-9981</p>
+        <p>Email: patlee@jcpcottage.com</p>
       </div>
       <div className="footer-links">
-        <p>This is the place we call home. This is the place we call home. This is the place we call home.</p>
+        <h3>Follow Us</h3>
+        <SocialLinks styleClass="footer-link" />
       </div>
     </div>
   </footer>
