8113b5888aa975e7f60bd89899741c4561bda8fe
toriecr <toriecr@gmail.com>
2020-12-07 23:30:39 -1000

    Add All Amenities Component

--- src/components/AllAmenities.js
+++ src/components/AllAmenities.js
@@ -1,10 +1,59 @@
 import React from "react"
 import Title from "./Title"
+import AllCare from "../constants/allCare"
+import { BiRightArrow } from "react-icons/bi"
+import { MdLocalLaundryService } from "react-icons/md"
+import { RiCommunityLine } from "react-icons/ri"
+import { FaWifi, FaPhone } from "react-icons/fa"
 
 const AllAmenities = () => {
   return <section className="all-amenities">
-    <Title title="All Amenities" />
+    <Title title="All Amenities and Services" />
     <div className="underline"></div>
+    <h2>Care Services</h2>
+    <div className="care-services">
+      {AllCare.map(amenity =>{
+        const { id, text } = amenity;
+        return (
+          <div key={id} className="care-service">
+            <p>
+              <BiRightArrow></BiRightArrow>
+              {text}
+            </p>
+          </div>
+        )
+      })}
+    </div>
+    <h2>Non-Care Services</h2>
+    <div className="non-care-services">
+      <div className="non-care-service">
+        <p>
+          <MdLocalLaundryService></MdLocalLaundryService>
+          Housekeeping and linen services
+        </p>
+      </div>
+      <div className="non-care-service">
+        <p>
+          <RiCommunityLine></RiCommunityLine>
+          Community-sponsored activities
+        </p>
+      </div>
+    </div>
+    <h2>Room Amenities</h2>
+    <div className="room-amenities">
+      <div className="room-amenity">
+        <p>
+          <FaPhone></FaPhone>
+          Telephone
+        </p>
+      </div>
+      <div className="room-amenity">
+        <p>
+          <FaWifi></FaWifi>
+          Wifi 
+        </p>
+      </div>
+    </div>
   </section>
 }
 
---
+++ src/constants/allCare.js
@@ -0,0 +1,42 @@
+import React from "react"
+
+const allCare = [
+  {
+    id: 1,
+    text: "Activities of daily living assistance"
+  },
+  {
+    id: 2,
+    text: "Assistance with dressing"
+  },
+  {
+    id: 3,
+    text: "Medication management"
+  },
+  {
+    id: 4,
+    text: "Transportation to medical appointments"
+  },
+  {
+    id: 5,
+    text: "Coordination with health care providers"
+  },
+  {
+    id: 6,
+    text: "24-hour supervision"
+  },
+  {
+    id: 7,
+    text: "Assistance with bathing"
+  },
+  {
+    id: 8,
+    text: "Assistance with transfers"
+  },
+  {
+    id: 9,
+    text: "Meal preparation and service"
+  }
+]
+
+export default allCare;
